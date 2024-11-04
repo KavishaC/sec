59719a4f7150f76d792177f5bc7a7b4a2a738371
Kavisha Chandraratne <kcchandraratne@gmail.com>
2024-11-04 17:18:12 +1100

    a

--- app/src/main.c
+++ app/src/main.c
@@ -52,14 +52,12 @@ void write_18bit_sample(FILE *file, uint32_t sample_18bit) {
     sample_18bit = sample_18bit << 6; // now in 24 bit format
     sample_18bit = sample_18bit << 8; // now in 32 bit format
 
-    sample_18bit ^= 0x80000000;
-
-    sample_18bit = sample_18bit >> 8; // back in 24 bit format
+    sample_18bit -= 0x80000000;
 
     // Write 18-bit sample packed into 24 bits
-    buffer[0] = (sample_18bit >> 16) & 0xFF;  // Most significant byte
-    buffer[1] = (sample_18bit >> 8) & 0xFF;   // Middle byte
-    buffer[2] = (sample_18bit >> 0) & 0xFF;   // Least significant byte (LSB)
+    buffer[0] = (sample_18bit >> 24) & 0xFF;  // Most significant byte
+    buffer[1] = (sample_18bit >> 16) & 0xFF;   // Middle byte
+    buffer[2] = (sample_18bit >> 8) & 0xFF;   // Least significant byte (LSB)
 
     // Write the 3-byte (24-bit) sample to the file
     fwrite(buffer, sizeof(buffer), 1, file);
